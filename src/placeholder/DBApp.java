e5c83ede666f6d184910643cff01917aa7b8f9fd
Reda Ramadan <king2sairos2013@gmail.com>
2018-02-10 22:24:57 +0200

    style: Add createTable method skeleton

--- src/placeholder/DBApp.java
+++ src/placeholder/DBApp.java
@@ -1,7 +1,46 @@
 package placeholder;
 
+import java.util.Hashtable;
+
 public class DBApp {
 	public void init() {
 
 	}
+
+	// TODO change Exception to DBAppException
+	public void createTable(String strTableName, Hashtable<String, String> htblColNameType) throws Exception {
+		if(checkMeta())
+		if (checkValidName(strTableName) && checkValidKeys(htblColNameType)) {
+			// TODO business logic table creation
+			addMetaData(strTableName,htblColNameType);
+			addDirectory(strTableName,"data");
+		} else {
+			throw new Exception();
+		}
+	}
+
+	private static boolean checkValidName(String strTableName) {
+		//TODO implement Name validation check
+		return true;
+	}
+
+	private static boolean checkValidKeys(Hashtable<String, String> htblColNameType) {
+		//TODO implement Key validation check
+		return true;
+	}
+	private static boolean checkMeta() {
+		//TODO implement meta data check
+		return true;
+	}
+	private static void addMetaData(String strTableName,Hashtable<String, String> htblColNameType) {
+		//TODO add meta data of table to file
+	}
+	private static void addDirectory(String directoryName, String filePath) {
+		//TODO add directory for table pages
+	}
+	
+	public static void main (String [] args) {
+		
+	}
+	
 }

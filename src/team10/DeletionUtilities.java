c643871f20ad11c47f9b4766dc52f00e81a7bd9b
OmarOES <omar.ohm.elsherif@gmail.com>
2018-02-25 08:28:16 +0200

    fix: Fixed the 3 bugs detected by Kilany in deletion
    1. Deletion is now based on the whole tuple in all situations.
    2. Correct page number is now used when serializing.
    3. Deletion now works cross page with no issues.

--- src/team10/DeletionUtilities.java
+++ src/team10/DeletionUtilities.java
@@ -1,5 +1,6 @@
 package team10;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Set;
@@ -12,56 +13,60 @@ public class DeletionUtilities {
 		Page currentPage = null;
 
 		// load and process table pages one by one until there are no more pages to load
-		while (true) {
+		boolean active = true;
+		while (active) {
 			try {
 				currentPage = PageManager.deserializePage("data/" + strTableName + "/page_" + pageNumber + ".ser");
 				// page loaded successfully
 				Hashtable<String, Object>[] rows = currentPage.getRows();
-
+				
+				// traversing the page row by row until a null row is encountered
 				for (int i = 0; i < currentPage.getMaxRows(); i++) {
 					Hashtable<String, Object> row = rows[i];
-
-					if (htblColNameValue.get(primaryKey) != null) {
-						// provided tuple had a primary key i.e. only a single row to delete
-						if (htblColNameValue.get(primaryKey).equals(row.get(primaryKey))
-								&& !(boolean) row.get("isDeleted")) {
-							// row found, delete and finalize
-							row.put("isDeleted", true);
-							deleteCount++;
-							PageManager.serializePage(currentPage,
-									"data/" + strTableName + "/page_" + pageNumber + ".ser");
-							System.out.printf("%d row(s) deleted\n", deleteCount);
-							return;
-						}
-					} else {
-						// provided tuple had no primary key i.e. multiple rows to delete
-						boolean match = true;
-						// checking current row against the given tuple
-						for (String tableKey : tableKeys) {
-							if (htblColNameValue.get(tableKey) != null
-									&& !htblColNameValue.get(tableKey).equals(row.get(tableKey))) {
-								match = false;
-								break;
-							}
-						}
-
-						if (match && !(boolean) row.get("isDeleted")) {
-							// current row matches the given tuple, proceed in deleting it
-							row.put("isDeleted", true);
-							deleteCount++;
+					if (row == null)
+						break;
+					boolean match = true;
+					boolean quit = false; // quits for loop in case a PK match is found
+					// checking for PK match
+					if (htblColNameValue.get(primaryKey) != null
+							&& htblColNameValue.get(primaryKey).equals(row.get(primaryKey))
+							&& !(boolean) row.get("isDeleted")) {
+						// current row matches a given primary key value, no more rows can match
+						active = false;
+						quit = true;
+					}
+					// checking current row against the given tuple
+					for (String tableKey : tableKeys) {
+						if (htblColNameValue.get(tableKey) != null
+								&& !htblColNameValue.get(tableKey).equals(row.get(tableKey))) {
+							match = false;
+							break;
 						}
 					}
+
+					if (match && !(boolean) row.get("isDeleted")) {
+						// current row matches the given tuple, proceed in deleting it
+						row.put("isDeleted", true);
+						deleteCount++;
+					}
+
+					if (quit)
+						break;
 				}
 
+				PageManager.serializePage(currentPage,
+						"data/" + strTableName + "/page_" + currentPage.getPageNumber() + ".ser");
 				pageNumber++;
 
-			} catch (Exception e) {
+			} catch (FileNotFoundException e) {
 				// there are no more pages in this table
-				break; // break out of the while loop to finalize
+				active = false; // break out of the while loop to finalize
+			} catch (Exception e) {
+				// unexpected exception
+				e.printStackTrace();
 			}
 		}
 		// finalization
-		PageManager.serializePage(currentPage, "data/" + strTableName + "/page_" + pageNumber + ".ser");
 		System.out.printf("%d row(s) deleted\n", deleteCount);
 	}
 }

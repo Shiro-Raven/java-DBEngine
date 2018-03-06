8e66c62b6264b2112bec380c27eea0a4f83344db
Reda Ramadan <king2sairos2013@gmail.com>
2018-03-06 08:21:51 +0200

    feat: Implement path and metadata check methods.
    The implemented methods are tableDirectoryExists, isColumnPrimary, and
    retrieveColumnMetaInTable.

--- src/team10/IndexUtilities.java
+++ src/team10/IndexUtilities.java
@@ -1,6 +1,9 @@
 package team10;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -8,20 +11,46 @@ public class IndexUtilities {
 
 	// Checks if the directory of table exists
 	protected static boolean tableDirectoryExists(String strTableName) throws DBAppException {
-
+		File tableFile = new File("data/" + strTableName);
+		if (tableFile.exists() && tableFile.isDirectory()) {
+			return true;
+		}
 		return false;
 	}
 
-	// Checks in the meta data whether the column is primary or not
-	protected static boolean isColumnPrimary(String columnMeta) {
-
+	// Checks in the meta data whether the column represents a primary key or not
+	protected static boolean isColumnPrimary(String columnMeta) throws DBAppException {
+		if (columnMeta == null) {
+			throw new DBAppException("meta data retreival error");
+		}
+		String[] columnParams = columnMeta.split(",");
+		if (columnParams[3].equals("true")) {
+			return true;
+		}
 		return false;
 
 	}
 
 	// Retrieves meta data of specific column in specific table
-	protected static String retrieveColumnInTable(String strTableName, String strColumnName) {
+	protected static String retrieveColumnMetaInTable(String strTableName, String strColumnName) {
+		BufferedReader metaReader;
+		try {
+			metaReader = new BufferedReader(new FileReader("data/metadata.csv"));
+			String metaLine = metaReader.readLine();
+			while (metaLine != null) {
+				String[] tableParams = metaLine.split(",");
+				if (tableParams[0].equals(strTableName) && tableParams[1].equals(strColumnName)) {
+					metaReader.close();
+					return metaLine;
+				}
+				metaLine = metaReader.readLine();
+			}
 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 
 	}
@@ -44,18 +73,18 @@ public class IndexUtilities {
 		File files = new File(filepath);
 
 		for (File file : files.listFiles()) {
-			
+
 			String name = file.getName();
-			if(name.substring(0, 6).equals("dense_") && name.substring(name.indexOf('.')).equals(".ser"))
+			if (name.substring(0, 6).equals("dense_") && name.substring(name.indexOf('.')).equals(".ser"))
 				pages.add(PageManager.deserializePage(file.getPath()));
-			
+
 		}
 
 		return pages;
 
 	}
 
-	// Checks a directory exists
+	// check a file path and create directories that don't exist through the file path on the file system
 	protected static void validateDirectory(String filepath) throws IOException {
 
 		String[] pathParams = filepath.split("/");

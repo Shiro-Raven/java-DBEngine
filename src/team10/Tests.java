4ca7acf806bf8b940942e339269c71a8d570353e
Maqarios <maqarioswagdy@gmail.com>
2018-03-19 21:03:57 +0200

    feat: add test cases for BRIN index

--- src/team10/Tests.java
+++ src/team10/Tests.java
@@ -56,18 +56,18 @@ public class Tests {
 		}
 
 	}
-	
+
 	static void testBRINIndex(String tblName, String colName) {
 
-		int denseIndexPageNumber = 1;
+		int BRINIndexPageNumber = 1;
 		Object lastValue = '\n';
 
 		while (true) {
 
-			Page denseIndexPage = PageManager.loadPageIfExists(
-					"data/" + tblName + "/" + colName + "/indices/BRIN/page_" + denseIndexPageNumber++ + ".ser");
+			Page BRINIndexPage = PageManager.loadPageIfExists(
+					"data/" + tblName + "/" + colName + "/indices/BRIN/page_" + BRINIndexPageNumber++ + ".ser");
 
-			if (denseIndexPage == null) {
+			if (BRINIndexPage == null) {
 
 				System.out.println("BRIN Check Is Complete!");
 				return;
@@ -75,32 +75,33 @@ public class Tests {
 			}
 
 			// Looping Through Rows Of EACH Index File
-			for (int i = 0; i < denseIndexPage.getMaxRows() && denseIndexPage.getRows()[i] != null; i++) {
+			for (int i = 0; i < BRINIndexPage.getMaxRows() && BRINIndexPage.getRows()[i] != null; i++) {
 
-				Hashtable<String, Object> denseIndexRow = denseIndexPage.getRows()[i];
+				Hashtable<String, Object> BRINIndexRow = BRINIndexPage.getRows()[i];
+				Page tempPage = PageManager.loadPageIfExists(
+						"data/" + tblName + "/page_" + ((int) BRINIndexRow.get("pageNumber")) + ".ser");
 
-				Page tablePage = PageManager.loadPageIfExists(
-						"data/" + tblName + "/page_" + ((Integer) denseIndexRow.get("pageNumber")) + ".ser");
+				int lastIndex;
+				for (lastIndex = -1; lastIndex < (tempPage.getMaxRows() - 1)
+						&& tempPage.getRows()[lastIndex + 1] != null; lastIndex++)
+					;
 
-				if (tablePage == null) {
+				if (!tempPage.getRows()[0].get(colName).equals(BRINIndexRow.get(colName + "Min")))
+					System.out.println("Error With Initial Values: " + BRINIndexRow + " & " + tempPage.getRows()[0]);
+				if (!tempPage.getRows()[lastIndex].get(colName).equals(BRINIndexRow.get(colName + "Max")))
+					System.out.println(
+							"Error With Final Values: " + BRINIndexRow + " & " + tempPage.getRows()[lastIndex]);
 
-					System.out.println("NOT FOUND: " + denseIndexRow);
-					continue;
+				if (BRINIndexRow.get(colName + "Min").toString().toString()
+						.compareTo(BRINIndexRow.get(colName + "Max").toString()) > 0)
+					System.out.println("ERROR WITH ORDER of Tuple: " + lastValue.toString() + " & "
+							+ BRINIndexRow.get(colName + "Max").toString());
 
-				}
-
-				// Checking Not Equality And Deletion
-				Hashtable<String, Object> tableRow = tablePage.getRows()[(Integer) denseIndexRow.get("locInPage")];
-				if (!denseIndexRow.get("value").equals(tableRow.get(colName)))
-					System.out.println("NOT EQUAL: " + denseIndexRow.get("value") + " & " + tableRow.get(colName));
-				if (!denseIndexRow.get("isDeleted").equals(tableRow.get("isDeleted")))
-					System.out.println("DELETION ERROR: " + denseIndexRow + " & " + tableRow);
-
-				// Checking Increasing Order Of Dense Indices
-				if (lastValue.toString().compareTo(denseIndexRow.get("value").toString()) > 0)
+				// Checking Increasing Order Of BRIN Indices
+				if (lastValue.toString().compareTo(BRINIndexRow.get(colName + "Max").toString()) > 0)
 					System.out.println("ERROR WITH ORDER: " + lastValue.toString() + " & "
-							+ denseIndexRow.get("value").toString());
-				lastValue = denseIndexRow.get("value");
+							+ BRINIndexRow.get(colName + "Max").toString());
+				lastValue = BRINIndexRow.get(colName + "Max");
 
 			}
 
@@ -138,7 +139,9 @@ public class Tests {
 		}
 
 		IndexUtilities.createDenseIndex("idTest", "first_name");
+
 		testDenseIndex("idTest", "first_name");
+		testBRINIndex("idTest", "first_name");
 
 	}
 
